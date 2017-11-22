e38f45c7de8c4f14ab2ad0b48f37969ad7056363
Jitse <jitsedevries@gmail.com>
2017-11-22 06:59:39 +0100

    templates

---
+++ views/views-view-unformatted--toptaken--block.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php foreach ($rows as $id => $row): ?>  
+    <div class="toptask">        
+        <?php print $row; ?>  
+    </div>
+<?php endforeach; ?>
+
+    
